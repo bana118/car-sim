1c482310a7de5760d946ef1a3c5ca18b7114757d
bana118 <tachibana.k.ae@m.titech.ac.jp>
2019-06-16 15:32:33 +0900

    add current speed viewer

---
+++ Carsim/Assets/Scripts/ViewCurrentSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using UnityEngine.UI;
+
+public class ViewCurrentSpeed : MonoBehaviour
+{
+    public CarController m_Car;
+    private Text speedViewer;
+    // Use this for initialization
+    private void Awake()
+    {
+        speedViewer = GetComponent<Text>();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentSpeed = (int)m_Car.CurrentSpeed;
+        speedViewer.text = currentSpeed.ToString() + "km/h";
+    }
+}
